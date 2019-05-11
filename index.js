19d3786f91517ea867c31dc8947301d8e3ea4551
Brostafa <mostafahefny2010@gmail.com>
2019-05-11 02:09:42 +0200

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 8080
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const LOGS = []
+
+app.post(`/gps`, (req, res) => {
+	const body = req.body || {}
+	const ip =  req.headers['x-forwarded-for'] || req.connection.remoteAddress
+	
+	res.send(`Received GPS Data - ip -> ${ip} & data body ${JSON.stringify(body)}`)
+	console.log(`Received data IP: ${ip} & BODY -> `, body)
+	LOGS.push(`Received data IP: ${ip} & BODY: ${JSON.stringify(body)}`)
+})
+
+app.get('/logs', (req, res) => {
+	const reloadScript = `<script>setTimeout(() => window.location.reload(), 5000)</script>`
+	const body = LOGS.slice(0).reverse().join('<br>')
+	const html = `
+	<!DOCTYPE html>
+	<html>
+		<head>
+		</head>
+		<body>
+			${body}
+			${reloadScript}
+		</body>
+	</html>
+	`
+	res.send(html)
+})
+
+app.listen(PORT, () => console.log(`[Server] Listening on PORT ${PORT}`))
